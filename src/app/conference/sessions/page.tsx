9d849b611beef445fa7a8d07a93e99a71b5a77d4
Cesar Whatley <julceswhat@gmail.com>
2023-09-12 17:04:10 -0500

    Adding fetching

--- src/app/conference/sessions/page.tsx
+++ src/app/conference/sessions/page.tsx
@@ -1,12 +1,64 @@
 import Link from "next/link";
 
-export default function Page() {
+import styles from "../conference.module.css";
+
+
+interface ISpeaker {
+  id: string;
+  name: string;
+}
+
+interface ISession {
+  id: string
+  title: string
+  description: string
+  room: string
+  day: string
+  track: string
+  speakers: ISpeaker[]
+}
+interface IData {
+  sessions: ISession[]
+}
+
+async function fetchSessions() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/sessions.json",
+    {
+      cache: 'no-store'
+    }
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+export default async function Page() {
+  const data: IData = await fetchSessions();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className={styles.parentContainer}>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>/Conference/Sessions</h1>
         <Link href={"/conference"}>Conference</Link>
+        <span>Last rendered: {new Date().toLocaleTimeString()}</span>
       </div>
-    </main>
+      <h1>Here are the Sessions!</h1>
+      {
+        data.sessions.map(({id, title, description, speakers, room, day, track}) => (
+          <div key={id} className={styles.infoContainer}>
+            <h3 className={styles.titleText}>{title}</h3>
+            {
+              speakers && speakers.map((sp) => (
+                <h3 key={sp.id} className={styles.titleText}>{sp.name}</h3>
+              ))
+            }
+            <h5 className={styles.descText}>{description}</h5>
+            <h4 className={styles.infoText}>Room: {room}</h4>
+            <h4 className={styles.infoText}>Day: {day}</h4>
+            <h4 className={styles.infoText}>Track: {track}</h4>
+          </div>
+        ))
+      }
+    </div>
   );
 }
--- src/app/conference/speakers/page.tsx
+++ src/app/conference/speakers/page.tsx
@@ -1,12 +1,47 @@
 import Link from "next/link";
 
-export default function Page() {
+import styles from "../conference.module.css";
+
+interface ISpeaker {
+  id: string;
+  bio: string;
+  name: string;
+}
+
+interface IData {
+  speakers: ISpeaker[];
+}
+
+async function fetchSpeakers() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json",
+    {
+      next: { revalidate: 20 },
+    }
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+export default async function Page() {
+  const data: IData = await fetchSpeakers();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className={styles.parentContainer}>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>/Conference/Speakers</h1>
         <Link href={"/conference"}>Conference</Link>
+        <span>Last rendered: {new Date().toLocaleTimeString()}</span>
       </div>
-    </main>
+      <h1>Welcome speakers</h1>
+      {data.speakers.map(({ id, name, bio }) => {
+        return (
+          <div key={id} className={styles.infoContainer}>
+            <h3 className={styles.titleText}>{name}</h3>
+            <h5 className={styles.descText}>{bio}</h5>
+          </div>
+        );
+      })}
+    </div>
   );
 }
